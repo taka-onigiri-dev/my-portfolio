16c83c8b2cdaf89185152f24bc839530ddc0e521
taka <toumyou463100@gmail.com>
2025-06-29 00:10:01 +0900

    Revert "多言語対応ファイルGitHub Pages動作確認"
    This reverts commit 87b0f8912fc4cae7233ec273493c93182930ab85.

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,14 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import { createI18nAsync } from './i18n'
+import i18n from './i18n'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-async function bootstrap() {
-  const i18n = await createI18nAsync()
-  const app = createApp(App)
-  app.use(i18n)
-  app.use(createPinia())
-  app.use(router)
-  app.mount('#app')
-}
+const app = createApp(App)
+app.use(i18n)
 
-bootstrap()
+app.use(createPinia())
+app.use(router)
+app.use(i18n)
+
+app.mount('#app')
